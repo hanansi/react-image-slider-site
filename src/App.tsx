dc1c7e1509b459bb85e14fd44b12bfaada019389
Aiden Castillo <aidencastillo41@gmail.com>
2025-09-10 12:27:34 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import ReactBeforeSliderComponent from 'react-before-after-slider-component';
+import 'react-before-after-slider-component/dist/build.css';
+import './App.css';
+
+function App() {
+  function getImages() {
+    const images: Record<string, string> = import.meta.glob("./assets/*", { eager: true, import: "default", query: "?url" });
+    const imagePaths = Object.values(images);
+    let content = [];
+
+    for (let i = 0; i + 2 <= imagePaths.length; i += 2) {
+      content.push(<div className="w-full max-w-sm mx-auto">
+                    <ReactBeforeSliderComponent 
+                        firstImage={{imageUrl: imagePaths[i]}} 
+                        secondImage={{imageUrl: imagePaths[i + 1]}} />
+                    </div>)
+    }
+
+    return content;
+  }
+
+  return (
+    <div className="flex flex-col gap-6 items-center my-4 w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12 mx-auto">{getImages()}</div>
+  );
+}
+
+
+export default App
