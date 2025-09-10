30a77f7e69ef5bb424ec969010544d8cf3594cd1
Aiden Castillo <aidencastillo41@gmail.com>
2025-09-10 12:47:15 -0600

    added base url for site deployment on github

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://hanansi.github.io/react-image-slider-site/",
   plugins: [react(),
   tailwindcss(),
   ],
